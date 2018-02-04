2c6334d29efcae34e3b94869f20ee10955226e6b
Berberich André <berbs1989@googlemail.com>
2018-02-04 14:21:36 +0100

    test

--- src/implementation/test/test/implementation/ConcurrentPrimeFinderTest.java
+++ src/implementation/test/test/implementation/ConcurrentPrimeFinderTest.java
@@ -8,7 +8,12 @@ import java.util.List;
 
 
 public class ConcurrentPrimeFinderTest {
-
+    @Test
+    public void isValid() throws Exception {
+        String value = "(5,7,29,47,59,61,67,79,83,89,269,463,467,487,569,599,859,883,887";
+        boolean retValue = Validator.isValid(value);
+        Assert.assertTrue(retValue);
+    }
 
     @Test
     public void isPrime_6_noPrime(){
---
+++ src/implementation/test/test/implementation/ValidatorTest.java
@@ -0,0 +1,20 @@
+package implementation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidatorTest {
+    @Test
+    public void isCandidate() throws Exception {
+    }
+
+    @Test
+    public void isValid() throws Exception {
+        String value = "(5,7,29,47,59,61,67,79,83,89,269,463,467,487,569,599,859,883,887";
+        boolean retValue = Validator.isValid(value);
+        Assert.assertTrue(retValue);
+    }
+
+}
