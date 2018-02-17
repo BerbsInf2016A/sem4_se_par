603b1c7ce1a7b2cbe009a4ba55beeaf87c197c1a
Berberich André <berbs1989@googlemail.com>
2018-02-17 17:39:40 +0100

    Updated bat + class files

--- 02_run2.bat
+++ 02_run2.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Java\jdk1.8.0_112\bin\java" -server -Xms6G -Xmx6G -XX:MaxDirectMemorySize=1024M -XX:NewSize=1G -XX:MaxNewSize=1G -XX:+UseParNewGC -XX:MaxTenuringThreshold=2 -XX:SurvivorRatio=8 -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=32768 -classpath build implementation.Main
+"C:\Program Files\Java\jdk1.8.0_112\bin\java" -server -Xms6G -Xmx6G -XX:MaxDirectMemorySize=1024M -XX:NewSize=1G -XX:MaxNewSize=1G -XX:+UseParNewGC -XX:MaxTenuringThreshold=2 -XX:SurvivorRatio=8 -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=32768 -classpath out\production\sem4_se_par implementation.Main
 pause
